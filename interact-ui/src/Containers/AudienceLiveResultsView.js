535fabbb5f1853e7a15ef907b66b43306f9ce57d
sagar chapara <sagarchapara@gmail.com>
2022-04-16 14:57:51 +0530

    added templte to call apis

--- interact-ui/src/Containers/AudienceLiveResultsView.js
+++ interact-ui/src/Containers/AudienceLiveResultsView.js
@@ -1,11 +1,13 @@
+import { useEffect, useState } from "react";
 import LiveResultsComponent from "../Components/LiveResultsComponent";
 
 const AudienceLiveResultsView = ()=>{
     let question = "Who is the Captain of Indian Cricket Team";
     let results = [{"option": "kohli","percentage": 20}, {"option": "Rohit","percentage": 50}, {"option": "Pant","percentage": 30}]
-
+    
+    //TODO: SocketIo integration
     return(
-        <LiveResultsComponent question={question} results={results} />
+        <LiveResultsComponent question={data.question} results={data.results} />
     );
 }   
 
--- interact-ui/src/Containers/AudienceView.js
+++ interact-ui/src/Containers/AudienceView.js
@@ -5,35 +5,42 @@ import {Button, Box} from '@mui/material'
 const AudienceView = () => {
     const [selected,setSelected] = useState(-1);
 
+    //TODO: need to pass this as a prop
+    const clientId = 1;
+
     let question = "Who is the Captain of Indian Cricket Team";
     let answers = ["Kohli", "Rohit","Pant"];
 
+    const [data, setData] = useState(null)
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(null)
+
+    useEffect(()=>{
+        fetch(`/fetchLiveQuestion/${clientId}`)
+        .then((response) => {
+            if(!response.ok){
+                throw new Error(
+                    `The status is ${response.status}`
+                )
+            }
+            return response.json
+        })
+        .then((data)=> {
+            setData(data)
+            setLoading(false)
+        })
+        .catch((error)=>{
+            setError(error)
+        })        
+    },[])
+
     return(
+        //TODO: handle loading and error states.
+
         <div>
         <MultipleChoiceQuestionCard 
-            question={question} 
-            choices={answers}
-            selected = {selected}
-            setSelected = {setSelected}
-        />
-
-        <MultipleChoiceQuestionCard 
-            question={question} 
-            choices={answers}
-            selected = {selected}
-            setSelected = {setSelected}
-        />
-
-        <MultipleChoiceQuestionCard 
-            question={question} 
-            choices={answers}
-            selected = {selected}
-            setSelected = {setSelected}
-        />
-
-        <MultipleChoiceQuestionCard 
-            question={question} 
-            choices={answers}
+            question={data.question} 
+            choices={data.answers}
             selected = {selected}
             setSelected = {setSelected}
         />
