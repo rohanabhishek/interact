ba31169bdd13b8e474f548597c6de8ffdb6c6fd0
sagar chapara <sagarchapara@gmail.com>
2022-04-16 15:37:36 +0530

    added socketio integration

--- interact-ui/src/Containers/AudienceLiveResultsView.js
+++ interact-ui/src/Containers/AudienceLiveResultsView.js
@@ -1,11 +1,29 @@
 import { useEffect, useState } from "react";
 import LiveResultsComponent from "../Components/LiveResultsComponent";
+import socket from "../socketio";
 
 const AudienceLiveResultsView = ()=>{
     let question = "Who is the Captain of Indian Cricket Team";
     let results = [{"option": "kohli","percentage": 20}, {"option": "Rohit","percentage": 50}, {"option": "Pant","percentage": 30}]
     
-    //TODO: SocketIo integration
+    //TODO: Loading and error handling
+    
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(()=>{
+        //for first time call server to add it to LiveResults room
+        socket.join("livePollResults")
+    },[])
+
+    //start listening to the live poll data
+    useEffect(()=>{
+        socket.on("livePollData", (data)=>{
+            setData(data)
+        })
+    },[socket])
+
     return(
         <LiveResultsComponent question={data.question} results={data.results} />
     );
---
+++ interact-ui/src/socketio.js
@@ -0,0 +1,6 @@
+import { io } from "socket.io-client";
+
+//TODO: give correct url
+const socket = io();
+
+export default socket;
